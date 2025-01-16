c4decd67f2c64d08eb1f42e1f7963fbb4fc115ee
bre3-19 <bre3_19@alu.uabcs.mx>
2025-01-16 13:44:30 -0700

    Diseño de Login y preparativos para el Controlador de Administrador

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -5,11 +5,26 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Requests\AuthAdminRequest;
+use App\Models\Order;
+use Carbon\Carbon;
 
 class AdminController extends Controller
 {
     public function index() {
-        return view ('admin.index');
+        $bookTotal = 0;
+        $bookTesis = 0;
+        $bookBooks = 0;
+        $bookTales = 0;
+        $loanToday = Order::whereDay('created_at', Carbon::today())->get();
+        $loanTotal = 0;
+        return view ('admin.index')->with([
+            'bookTotal' => $bookTotal,
+            'bookTesis' => $bookTesis,
+            'bookBooks' => $bookBooks,
+            'bookTales' => $bookTales,
+            'loanToday' => $loanToday,
+            'loanTotal' => $loanTotal,
+        ]);
     }
 
     public function login() {
--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -7,26 +7,33 @@
     </head>
     <body>
         <div class="flex flex-col w-full md:w-1/2 xl:w-2/5 2xl:w-2/5 3xl:w-1/3 mx-auto p-8 md:p-10 2xl:p-12 3xl:p-14 bg-[#ffffff] rounded-2xl shadow-xl">
-            <div class="flex flex-row gap-3 pb-4">
-                @session('error')
-                    <div class="alert alert-danger my-2">
-                        {{ session('error') }}
-                    </div>
-                @endsession
+        
+        <!--Mensaje de error-->
+        @session('error')
+            <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative gap-4" role="alert">
+                <strong class="font-bold">Error!</strong>
+                <span class="block sm:inline">{{ session('error') }}</span>
+                <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+                    <svg class="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
+                </span>
+            </div>
+        @endsession    
+
+        <div class="flex flex-row gap-3 pb-4 mt-4">
                 <div>
-                    <img src="/favicon.svg" alt="Logo" width="100">
+                    <img src="{{url('/images/logo.png')}}" alt="Logo" width="100">
                 </div>
                 <!---->
-                <h1 class="text-3xl font-bold text-[231f20] text-[231f20] my-auto">UPN</h1>
+                <h1 class="text-3xl font-bold text-[231f20] text-[231f20] my-auto">Universidad Pedagógica Nacional</h1>
             </div>
-            <div class="text-sm font-light text-[#6B7280] pb-8 ">Login to your account on Your Company.</div>
+            <div class="text-base text-center font-light text-[#6B7280] pb-8 ">Accede a la cuenta de administrador.</div>
             <!---->
             <form class="flex flex-col" action="{{ route('admin.auth') }}" method="POST">
                 @csrf
                 <div class="pb-2">
-                    <label for="email" class="block mb-2 text-base font-medium text-[#111827]">Email</label>
+                    <label for="email" class="block mb-2 text-base font-medium text-[#111827]">Correo electrónico</label>
                     <div class="relative text-gray-400"><span class="absolute inset-y-0 left-0 flex items-center p-1 pl-3"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-mail"><rect width="20" height="16" x="2" y="4" rx="2"></rect><path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path></svg></span> 
-                        <input type="email" name="email" id="email" class="pl-12 mb-2 bg-gray-50 text-gray-600 border focus:border-transparent border-gray-300 sm:text-sm rounded-lg ring ring-transparent focus:ring-1 focus:outline-none focus:ring-gray-400 block w-full p-2.5 rounded-l-lg py-3 px-4 @error('email') is-invalid @enderror" placeholder="name@company.com" autocomplete="off">
+                        <input type="email" name="email" id="email" class="pl-12 mb-2 bg-gray-50 text-gray-600 border focus:border-transparent border-gray-300 sm:text-sm rounded-lg ring ring-transparent focus:ring-1 focus:outline-none focus:ring-gray-400 block w-full p-2.5 rounded-l-lg py-3 px-4 @error('email') is-invalid @enderror" placeholder="email@gmail.com" autocomplete="off">
                     </div>
                     <div class="relative text-gray-400"><span class="absolute inset-y-0 left-0 flex items-center p-1 pl-3"></span> 
                         @error('email')
@@ -37,7 +44,7 @@
                     </div>
                 </div>
                 <div class="pb-6">
-                    <label for="password" class="block mb-2 text-base font-medium text-[#111827]">Password</label>
+                    <label for="password" class="block mb-2 text-base font-medium text-[#111827]">Contraseña</label>
                     <div class="relative text-gray-400"><span class="absolute inset-y-0 left-0 flex items-center p-1 pl-3"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-square-asterisk"><rect width="18" height="18" x="3" y="3" rx="2"></rect><path d="M12 8v8"></path><path d="m8.5 14 7-4"></path><path d="m8.5 10 7 4"></path></svg></span> 
                         <input type="password" name="password" id="password" placeholder="••••••••••" class="pl-12 mb-2 bg-gray-50 text-gray-600 border focus:border-transparent border-gray-300 sm:text-sm rounded-lg ring ring-transparent focus:ring-1 focus:outline-none focus:ring-gray-400 block w-full p-2.5 rounded-l-lg py-3 px-4 @error('contrasena') is-invalid @enderror" autocomplete="new-password">
                     </div>
@@ -49,8 +56,8 @@
                         @enderror
                     </div>
                 </div>
-                <button type="submit" class="w-full text-[#FFFFFF] bg-[#115cab] focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mb-6">Login</button>
-                <div class="text-sm font-light text-[#6B7280] text-center">Don't have an accout yet? <a href="#" class="font-medium text-[005caa] hover:underline">Sign Up</a>
+                <button type="submit" class="w-full text-[#FFFFFF] bg-[#115cab] focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-base px-5 py-2.5 text-center mb-6">Acceder</button>
+                <div class="text-sm font-light text-[#6B7280] text-center">No tienes una cuenta? <a href="#" class="font-medium text-[005caa] hover:underline">Registrate</a>
                 </div>
             </form>
             <!---->
